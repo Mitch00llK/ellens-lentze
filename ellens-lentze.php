1b6fa07d900e8d287a418679c04868a570448bbf
mitchellsl <mitch@sociallane.nl>
2026-01-08 11:31:00 +0100

    initial commit

---
+++ ellens-lentze.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: SocialLane Components
+ * Description: Custom components and functionality for SocialLane.
+ * Version: 1.0.0
+ * Author: Mitch
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
